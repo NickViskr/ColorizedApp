b7d6d6a91e53ead72cfa1187ae6396c1dee5d57e
NickViskr <nviskr@gmail.com>
2022-06-25 23:55:15 +0500

    item customization

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorArea: UIView!
+    
+    @IBOutlet var sliderRedColor: UISlider!
+    @IBOutlet var sliderGreenColor: UISlider!
+    @IBOutlet var sliderBlueColor: UISlider!
+    
+    
+    @IBOutlet var redColorLevel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorArea.layer.cornerRadius = 15
+        setupSlider()
+        
+
     }
 
-
+    @IBAction func redColorChange() {
+        
+        //colorArea.backgroundColor = UIColor(red: CGFloat(sliderRedColor.value), green: CGFloat(sliderGreenColor.value), blue: CGFloat(sliderBlueColor), alpha: 1)
+    }
+    
+    private func setupSlider() {
+        sliderRedColor.value = 0.5
+        sliderRedColor.minimumValue = 0
+        sliderRedColor.maximumValue = 1
+        sliderRedColor.minimumTrackTintColor = .red
+        sliderRedColor.maximumTrackTintColor = .white
+        sliderRedColor.thumbTintColor = .green
+    }
 }
 
