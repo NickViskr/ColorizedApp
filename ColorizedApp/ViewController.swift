a19cbd0810b3f7ecc1c2a122c89783837c3df492
NickViskr <nviskr@gmail.com>
2022-07-13 00:19:45 +0500

    Completely redone

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -19,11 +19,9 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorLevel: UILabel!
     @IBOutlet var blueColorLevel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorArea.layer.cornerRadius = 15
-        setupSlider()
         setupTexLabel()
         colorChange()
         labelValueSetting()
@@ -38,30 +36,6 @@ class ViewController: UIViewController {
         redColorLevel.text = String(sliderRedColor.value)
         greenColorLevel.text = String(sliderGreenColor.value)
         blueColorLevel.text = String(sliderBlueColor.value)
-    }
-    
-    private func setupSlider() {  // этот код можно заменить выставлением значений в сториборде
-        sliderRedColor.value = 0.5
-        sliderRedColor.minimumValue = 0
-        sliderReedColor.maximumValue = 1
-        sliderRedColor.minimumTrackTintColor = .red
-        sliderRedColor.maximumTrackTintColor = .white
-        sliderRedColor.thumbTintColor = .green
-
-        sliderGreenColor.value = 0.5
-        sliderGreenColor.minimumValue = 0
-        sliderGreenColor.maximumValue = 1
-        sliderGreenColor.minimumTrackTintColor = .green
-        sliderGreenColor.maximumTrackTintColor = .white
-        sliderGreenColor.thumbTintColor = .green
-
-        sliderBlueColor.value = 0.5
-        sliderBlueColor.minimumValue = 0
-        sliderBlueColor.maximumValue = 1
-        sliderBlueColor.minimumTrackTintColor = .blue
-        sliderBlueColor.maximumTrackTintColor = .white
-        sliderBlueColor.thumbTintColor = .green
-
     }
     
     private func colorChange() {
